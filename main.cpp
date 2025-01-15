04e9cbe3bbd63a27ef9094c2e11cdb91c6cd305a
marvelaiz <marianagabriela.a98@gmail.com>
2025-01-15 14:18:59 +0000

    init EMW3080 driver initialized

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include "mbed.h"
 
+
 // Blinking rate in milliseconds
 #define BLINKING_RATE     1000ms
 #include "EMW3080BInterface.h"
+#include "TCPSocket.h"
 
+
+
+#define EMW3080B_WIFI_DEBUG                                         0                                                                                                // set by library:emw3080b
+#define EMW3080B_WIFI_FLOW                                          PG_15                                                                                            // set by library:emw3080b[B_U585I_IOT02A]
+#define EMW3080B_WIFI_MISO                                          PD_3                                                                                             // set by library:emw3080b[B_U585I_IOT02A]
+#define EMW3080B_WIFI_MOSI                                          PD_4                                                                                             // set by library:emw3080b[B_U585I_IOT02A]
+#define EMW3080B_WIFI_NOTIFY                                        PD_14                                                                                            // set by library:emw3080b[B_U585I_IOT02A]
+#define EMW3080B_WIFI_NSS                                           PB_12                                                                                            // set by library:emw3080b[B_U585I_IOT02A]
+#define EMW3080B_WIFI_RESET                                         PF_15                                                                                            // set by library:emw3080b[B_U585I_IOT02A]
+#define EMW3080B_WIFI_RX                                            NC                                                                                               // set by library:emw3080b
+#define EMW3080B_WIFI_SCLK                                          PD_1                                                                                             // set by library:emw3080b[B_U585I_IOT02A]
+#define EMW3080B_WIFI_SPI_INTERFACE                                 1                                                                                                // set by library:emw3080b[B_U585I_IOT02A]
+#define EMW3080B_WIFI_TX                                            NC   
 // Definitions ---------------------------------------------------------
 
 // Change it with your WiFi network name
@@ -16,6 +31,25 @@ int main()
 {
     // Initialise the digital pin LED1 as an output
     DigitalOut led(LED1);
+
+    int count = 0;
+
+    EMW3080BInterface wifi(EMW3080B_WIFI_DEBUG,
+                      EMW3080B_WIFI_MOSI,
+                      EMW3080B_WIFI_MISO,
+                      EMW3080B_WIFI_SCLK,
+                      EMW3080B_WIFI_NSS,
+                      EMW3080B_WIFI_NOTIFY,
+                      EMW3080B_WIFI_FLOW,
+                      EMW3080B_WIFI_RESET,
+                      EMW3080B_WIFI_TX,
+                      EMW3080B_WIFI_RX,
+                      EMW3080B_EMAC::get_instance(),
+                      OnboardNetworkStack::get_default_instance()
+                     );
+
+
+    
 
     while (true) {
         printf("Blink!\n");
